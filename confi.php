634c799f1f9e8e1424d44306bb3892ee2a8995d5
Guillem Simó <gsimolandete@insalba.cat>
2016-11-24 19:56:04 +0100

    Delete confi.php

--- confi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_host = "localhost";
-$db_username = "root"; 
-$db_pass = ""; 
-$db_name = "tdrtest";
-
-$conn = mysqli_connect("$db_host", "$db_username", "$db_pass");
-mysqli_select_db($conn,"$db_name");
